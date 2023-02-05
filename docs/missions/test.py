d013c18cef0ac169fcfa338d8878b8303dac3604
botkero <lurexon@outlook.de>
2023-02-05 12:01:31 +0100

    move mission pages

--- docs/missions/test.py
+++
@@ -1,9 +0,0 @@
-data = []
-with open ("index.md", "r") as f:
-    data = f.readlines()
-    print(data[0])
-
-with open ("index.md", "w") as f:
-    for i in range (len(data)):
-        d = data[i].replace("-", "")
-        f.write(str(i)+ ". " + d)
